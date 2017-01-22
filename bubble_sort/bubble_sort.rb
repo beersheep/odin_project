382a77c4a8665b1b72d61ff4c0edd252f197f49a
Roy Huang <beersheep0109@gmail.com>
2017-01-22 15:28:18 +0800

    finish bubble sort by

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -7,8 +7,16 @@ class Array
 
     while self.any? do 
       self.each_with_index do |n, i|
-        if (n <=> self[i + 1]) == 1 
-          self[i], self[i + 1] = self[i + 1], self[i]
+        unless self[i + 1].nil?
+          if block_given? 
+            comparison = yield(n, self[i + 1])
+          else 
+            comparison = (n <=> self[i + 1])
+          end
+            
+          if comparison >= 1 
+            self[i], self[i + 1] = self[i + 1], self[i]
+          end
         end
       end
 
@@ -17,6 +25,8 @@ class Array
     
     final_arr
   end
+
+  alias_method :bubble_sort_by, :bubble_sort
 end
 
 
--- bubble_sort/bubble_sort_test.rb
+++ bubble_sort/bubble_sort_test.rb
@@ -5,7 +5,7 @@ require_relative 'bubble_sort.rb'
 class TestBubbleSort < Minitest::Test
 
   def test_it_has_bubble_sort_method
-    assert_equal(true, Array.new.respond_to?(:bubble_sort))
+    assert_respond_to(Array.new, :bubble_sort)
   end
 
   def test_bubble_sort_1
@@ -21,6 +21,54 @@ class TestBubbleSort < Minitest::Test
   def test_bubble_sort_3
     array = (1..100).to_a.shuffle
     assert_equal((1..100).to_a, array.bubble_sort)
+  end
+
+  def test_it_has_bubble_sort_by_method
+    assert_respond_to(Array.new, :bubble_sort_by)
+  end
+
+  def test_bubble_sort_by_1
+    array = ["hi","hello","hey"]
+    result = array.bubble_sort_by do |left, right|
+      left.length - right.length
+    end
+
+    expected = ["hi", "hey", "hello"]
+
+    assert_equal(expected, result)
+  end
+
+  def test_bubble_sort_by_2
+    array = ["roy", "cookie", "kyle", "vicente"]
+    result = array.bubble_sort_by do |left, right|
+      left.slice(0) <=> right.slice(0)
+    end
+
+    expected = ["cookie", "kyle", "roy", "vicente"]
+
+    assert_equal(expected, result)
+  end
+
+  def test_bubble_sort_by_3
+    array = [
+      { item: "trousers", price: 450 }, 
+      { item: "blazer", price: 1200 }, 
+      { item: "shirt", price: 300 }, 
+      { item: "jeans", price: 120 }
+    ]
+
+    result = array.bubble_sort_by do |left, right|
+      right[:price] - left[:price]
+    end
+
+    expected = [
+      { item: "blazer", price: 1200 }, 
+      { item: "trousers", price: 450 }, 
+      { item: "shirt", price: 300 }, 
+      { item: "jeans", price: 120 }
+    ]
+
+    assert_equal(expected, result)
   end
 end
 
