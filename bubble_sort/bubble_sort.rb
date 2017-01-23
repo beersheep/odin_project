70d4e3f83daf064841c3908ff2735a119d718316
Roy Huang <beersheep0109@gmail.com>
2017-01-23 10:09:52 +0800

    improve algorithm

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,20 +1,22 @@
 class Array 
   def bubble_sort
     this = self.clone
-    final_arr = []
+    should_swap = true
 
-    while this.any? do 
+    while should_swap do 
+      should_swap = false
       this.each_with_index do |n, i|
         unless this[i + 1].nil?
           comparison = block_given? ? yield(n, this[i + 1]) : (n <=> this[i+1])
-          this[i], this[i + 1] = this[i + 1], this[i] if comparison >= 1 
+          if comparison >= 1 
+            this[i], this[i + 1] = this[i + 1], this[i]
+            should_swap = true
+          end
         end
       end
-
-      final_arr.unshift(this.pop)
     end
 
-    final_arr
+    this
   end
 
   alias_method :bubble_sort_by, :bubble_sort
